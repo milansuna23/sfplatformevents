f9502a74a1de5060cf19bbf7d854a62f9bc0a3fc
milansuna23 <milancet2010@gmail.com>
2018-12-06 13:32:17 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var express = require('express');
 var jsforce = require('jsforce');
-var app = express().get('/', renderHome);
+var app = express();
 var server = require('http').Server(app);
 const PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
 server.listen(PORT);
