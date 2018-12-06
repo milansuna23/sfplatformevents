eb2576c0b27e92a619f65ec9b44aed7ed5504775
milansuna23 <milancet2010@gmail.com>
2018-12-06 10:04:04 +0100

    Initial version

---
+++ app.js
@@ -0,0 +1,10 @@
+var jsforce = require('jsforce');
+var username = 'test-i9batbfndswe@example.com';
+var password = '5$9JQgA5S7';
+var conn = new jsforce.Connection({});
+conn.login(username, password, function(err, userInfo) {
+  if (err) { return console.error(err); }
+  conn.streaming.topic("/event/Partner_Lead_Event__e").subscribe(function(message) {
+    console.dir(message);
+  });
+});
