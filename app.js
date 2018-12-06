0df785f8bca9eef4c182dc39d75dd1cb5355a7c5
milansuna23 <milancet2010@gmail.com>
2018-12-06 11:18:47 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,6 +1,6 @@
 var jsforce = require('jsforce');
 var username = 'test-i9batbfndswe@example.com';
-var password = '5$9JQgA5S7';
+var password = '5$9JQgA5S7J7JteRuM8fFDzSxC0iwMZceIG';
 var conn = new jsforce.Connection({});
 conn.login(username, password, function(err, userInfo) {
   if (err) { return console.error(err); }
