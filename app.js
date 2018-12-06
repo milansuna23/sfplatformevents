b9d4608940ace5b2c43cca333f63189eef7b041c
milansuna23 <milancet2010@gmail.com>
2018-12-06 11:29:37 +0100

    Update app.js

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 var jsforce = require('jsforce');
 var username = 'test-i9batbfndswe@example.com';
 var password = '5$9JQgA5S7J7JteRuM8fFDzSxC0iwMZceIG';
-var conn = new jsforce.Connection({});
+var conn = new jsforce.Connection({loginUrl:'https://test.salesforce.com'});
 conn.login(username, password, function(err, userInfo) {
   if (err) { return console.error(err); }
   conn.streaming.topic("/event/Partner_Lead_Event__e").subscribe(function(message) {
