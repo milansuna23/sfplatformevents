d833c86ad45fbcd2d42fdac3e93bc3921f357119
milansuna23 <milancet2010@gmail.com>
2018-12-10 11:56:52 +0100

    Update oAuth.js

--- oAuth.js
+++ oAuth.js
@@ -5,6 +5,7 @@ process.env.CLIENT_ID='3MVG9w8uXui2aB_pAtfoLESLXYEXcOJIMvTFcYLYkVddlAUCbvsvxYANY
 process.env.CLIENT_SECRET_ID='5779680760685351853';
 process.env.REDIRECT_URI='https://sfplatformevents.herokuapp.com/oauth/_callback';
 process.env.LOGIN_SERVER='https://test.salesforce.com';
+process.env.SALESFORCE_API_VERSION='43.0'
 var app = express();
 var server = require('http').Server(app);
 const PORT = process.env.PORT || 3001; // use heroku's dynamic port or 3001 if localhost
@@ -31,7 +32,7 @@ app.get('/oauth/_callback', function(req,res) {
     loginUrl: process.env.LOGIN_SERVER,
     redirectUri: process.env.REDIRECT_URI
   });
-  const conn = new jsforce.Connection({ oauth2 : oauth2 });
+  const conn = new jsforce.Connection({ oauth2 : oauth2, version : process.env.SALESFORCE_API_VERSION });
   conn.authorize(req.query.code, function(err, userInfo) {
     if (err) {
       return console.error(err);
@@ -47,8 +48,79 @@ app.get('/oauth/_callback', function(req,res) {
     }); */
     
   });
-  res.render(path.join(__dirname, 'pages/home'));
+  //res.render(path.join(__dirname, 'pages/home'));
+  subscribeToEvents( conn, res );
 });
+
+
+function subscribeToEvents( sfClient, res ) {
+
+    console.log( 'subscribing to events...' );
+
+    // http://paulbattisson.com/blog/2017/consuming-platform-events-in-10-lines-of-javascript/
+    sfClient.streaming.topic( '/event/Swag_Level__e' ).subscribe( function( message ) {
+
+        console.log( '-- RECEIVED EVENT -----------------------------------------------' );
+        console.log( message );
+
+    });
+
+    res.redirect( '/subscribe?accessToken=' + sfClient.accessToken + '&instanceUrl=' + sfClient.instanceUrl );
+
+}
+
+
+
+app.get( '/subscribe', function( req, res ) {
+
+    // should probably use a session store
+    // to keep up with the access token and instance urls
+    // per user; for simplicity (and not very secure)
+    // just passing around as URL parameters
+
+    res.render( 'subscribe', {
+        'accessToken' : req.query.accessToken,
+        'instanceUrl' : req.query.instanceUrl,
+        'version' : req.query.version || process.env.SALESFORCE_API_VERSION
+    });
+
+});
+
+app.get( '/publish', function( req, res ) {
+
+    console.log( 'publishing new event...' );
+
+    var sfClient = new jsforce.Connection({
+        instanceUrl : req.query.instanceUrl,
+        accessToken : req.query.accessToken,
+        version : req.query.version
+    });
+
+    sfClient.sobject( 'Partner_Lead_Event__e' ).create({
+
+        'Lead_Id__c' : 'SFLeadId';
+
+    }).then( function( result ) {
+
+        console.log( result );
+        res.redirect( '/subscribe?accessToken=' + sfClient.accessToken + '&instanceUrl=' + sfClient.instanceUrl );
+
+    }).catch( function( err ) {
+
+        handleError( err );
+
+    });
+
+});
+
+function handleError( err, res ) {
+
+    console.error( err );
+
+    res.status( 403 ).send( err );
+
+};
+
 
 
 
